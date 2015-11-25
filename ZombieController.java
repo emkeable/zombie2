01067a257eb99fbf339f352b790fa334a9fa3ab1
emkeable <emilykeable@gwu.edu>
2015-11-24 19:17:41 -0500

    Create ZombieController.java

---
+++ ZombieController.java
@@ -0,0 +1,87 @@
+package zombie;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.Timer;
+
+public class ZombieController implements MouseListener {
+
+	private final ZombieModel model;
+	private final ZombieView view;
+	private final int delay;
+
+	public ZombieController(ZombieModel modelArg, ZombieView viewArg, int sleepTimeArg) {
+		model = modelArg;
+		view = viewArg;
+		delay = sleepTimeArg;
+	}
+
+	public void beginSimulation() {
+		model.initialize();
+		view.draw();
+		ActionListener taskPerformer = new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				model.update();
+				view.draw();
+			}
+		};
+		new Timer(delay, taskPerformer).start();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+		if((e.getX()>200) && (e.getX()<600) &&(e.getY()<200) && (e.getY()>0))
+				{
+					model.getaZombie().setDirection("North");
+				}
+		else if((e.getX()>200) && (e.getX()<600) &&(e.getY()>400) && (e.getY()<800))
+		{
+			
+			model.getaZombie().setDirection("South");
+		}
+		else if((e.getX()>600) && (e.getX()<800) &&(e.getY()>0) && (e.getY()<400))
+		{
+			
+			model.getaZombie().setDirection("East");
+		}
+		else if((e.getX()<200) && (e.getX()>0) &&(e.getY()>0) && (e.getY()<400))
+		{
+			
+			model.getaZombie().setDirection("West");
+		}
+	}
+	
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+		
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+	
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
